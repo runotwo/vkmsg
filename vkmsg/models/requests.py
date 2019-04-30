f0d809973d9fb3fe806d92e10e77e063699ac171
runotwo <dantonov@dengabank.ru>
2019-04-30 10:52:04 +0300

    add attachments

--- vkmsg/models/requests.py
+++ vkmsg/models/requests.py
@@ -1,3 +1,5 @@
+import requests
+
 class IncomingMessage(object):
     def __init__(self, **kwargs):
         for key in kwargs:
@@ -10,6 +12,30 @@ class IncomingRequest(object):
         self.group_id = group_id
         dialog_messages = ['message_new', 'message_reply', 'message_edit']
         for key in kwargs:
+            if key == 'attachments':
+                self.attachments = [Attachment(x) for x in kwargs[key]]
             setattr(self, key, kwargs[key])
         if self.type in dialog_messages:
             self.object = IncomingMessage(**self.object)
+
+
+
+class Attachment(object):
+    def __init__(self, type, **kwargs):
+        self.type = type
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
+        if self.type == 'photo':
+            self.photo = Photo(**self.photo)
+
+
+class Photo(object):
+    def __init__(self, **kwargs):
+        for key in kwargs:
+            if key == 'sizes':
+                self.image_url = kwargs[key][-1]['url']
+                continue
+            setattr(self, key, kwargs[key])
+
+    def get_image(self):
+        return requests.get(self.image_url).content
